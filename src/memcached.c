f41b98c620580baae29ff3e17155b6faa87f2a72
CSenshi <saba.pochkhua@gmail.com>
2019-06-21 16:14:52 +0400

    implemented add and get key

--- src/memcached.c
+++ src/memcached.c
@@ -7,9 +7,17 @@
 #include <netinet/ip.h>
 
 #include "debug.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+
+char *_create_struct_request(char *command, mm_data_info info);
+char *_create_str_request(char *command, char *str);
+
+void _append_ending(char **str);
+
+void _send_mm_req(int fd, char *req);
+char *_recv_mm_resp(int fd);
 
 void memcached_init(struct memcached *m)
 {
@@ -29,8 +37,103 @@ void memcached_init(struct memcached *m)
     _debug_print("Connection Succesful!\n");
 }
 
+int memcached_add(struct memcached *m, struct mm_data_info info, char *value)
+{
+    char *req = _create_struct_request("add", info);
+    char *val = strdup(value);
+
+    _append_ending(&req);
+    _append_ending(&val);
+
+    _send_mm_req(m->fd, req);
+    _send_mm_req(m->fd, val);
+
+    char *res = _recv_mm_resp(m->fd);
+    // TODO error Handling (enum)
+    if (!strcmp(res, ADD_SUCCESS))
+        return 0;
+    else
+        return -1;
+}
+
+char *memcached_get(struct memcached *m, char *key)
+{
+    char *req = _create_str_request("get", key);
+    _append_ending(&req);
+
+    _send_mm_req(m->fd, req);
+    //TODO error Handling
+    char *res = _recv_mm_resp(m->fd);
+}
+
 void memcached_exit(struct memcached *m)
 {
     _debug_print("Closing Connection...\n");
     close(m->fd);
+}
+
+void _send_mm_req(int fd, char *req)
+{
+    // TODO iterate with while cycle
+    write(fd, req, strlen(req));
+    _debug_print("Request Sent : %s", req);
+}
+
+char *_recv_mm_resp(int fd)
+{
+    int buff_size = 100;
+    char *buffer = malloc(sizeof(char) * buff_size);
+    // TODO iterate with while
+    int t = read(fd, buffer, buff_size);
+    buffer[t] = '\0';
+    _debug_print("Response Recieved : %s", buffer);
+    return buffer;
+}
+
+char *_create_str_request(char *command, char *str)
+{
+    int to_alloc = strlen(command) + strlen(str) + 1;
+    char *req = malloc(to_alloc);
+
+    strcat(req, command);
+    strcat(req, " ");
+    strcat(req, str);
+
+    return req;
+}
+
+char *_create_struct_request(char *command, mm_data_info info)
+{
+    char flags_str[50];
+    sprintf(flags_str, "%d", info.flags);
+
+    char ttl_str[50];
+    sprintf(ttl_str, "%d", info.ttl);
+
+    char size_str[50];
+    sprintf(size_str, "%d", info.size);
+
+    int to_alloc = strlen(command) + strlen(info.key) + strlen(flags_str) + strlen(ttl_str) + strlen(size_str) + 7;
+    char *req = malloc(to_alloc);
+
+    strcat(req, command);
+    strcat(req, " ");
+
+    strcat(req, info.key);
+    strcat(req, " ");
+
+    strcat(req, flags_str);
+    strcat(req, " ");
+
+    strcat(req, ttl_str);
+    strcat(req, " ");
+
+    strcat(req, size_str);
+    return req;
+}
+
+void _append_ending(char **str)
+{
+    *str = realloc(*str, strlen(*str) + 2);
+    strcat(*str, "\r\n");
 }
--- src/memcached.h
+++ src/memcached.h
@@ -1,6 +1,8 @@
 #ifndef MEMCACHED_H
 #define MEMCACHED_H
 
+#include <stdio.h>
+
 typedef struct memcached
 {
     char *addr;
@@ -8,7 +10,21 @@ typedef struct memcached
     int fd;
 } memcached;
 
+typedef struct mm_data_info
+{
+    char *key;
+    int flags;
+    int ttl;
+    int size;
+} mm_data_info;
+
 void memcached_init(struct memcached *m);
+
+/* Set a key unconditionally */
+int memcached_add(struct memcached *m, struct mm_data_info, char *value);
+
+/* Reads a value */
+char *memcached_get(struct memcached *m, char *key);
 
 void memcached_exit(struct memcached *m);
 
