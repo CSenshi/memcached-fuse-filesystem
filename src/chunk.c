862536b9fd7c54cb60f4cbced09980231c5af4d5
CSenshi <saba.pochkhua@gmail.com>
2019-07-08 03:05:56 +0400

    add functionality to write in the chunk

--- src/chunk.c
+++ src/chunk.c
@@ -8,16 +8,50 @@ int chunk_create(memcached *m)
     chunk *new_chunk = malloc(sizeof(chunk));
     chunk_init(new_chunk, m);
 
-    int res = memcached_add_struct(m, int_to_str(new_chunk->inode), new_chunk, sizeof(chunk));
+    int res = memcached_add_struct(m, int_to_str(new_chunk->inode), new_chunk, sizeof(struct chunk));
+
+    if (res == ERROR || res == NOT_STORED)
+        return -1;
 
     return new_chunk->inode;
 }
 
 void chunk_init(chunk *chunk, memcached *m)
 {
+    // fill with zeros
+    memset(chunk, 0, sizeof(struct chunk));
+
+    chunk->_NOT_USED = -1;
     chunk->inode = get_next_index(m);
     chunk->ind = 0;
-    chunk->next_chunk_inode = 0;
+    chunk->next_chunk_inode = -1;
+}
 
-    memset(chunk->data, 0, DATA_SIZE);
+int chunk_write(chunk *c, void *data, int size, memcached *m)
+{
+    if (c->ind + size < DATA_SIZE)
+    {
+        memcpy(c->data + c->ind, data, size);
+        c->ind += size;
+    }
+    else
+    {
+        // ToDo: Implement linked list chunk
+    }
+
+    int res = memcached_replace_struct(m, int_to_str(c->inode),
+                                       c, sizeof(struct chunk));
+    return 0;
+}
+
+chunk *chunk_mmch_getchunk(int inode, memcached *m)
+{
+    char *key = int_to_str(inode);
+    mm_data_info info = memcached_get(m, key);
+
+    // copy given value into
+    chunk *c = malloc(sizeof(struct chunk));
+    memcpy(c, info.value, sizeof(struct chunk));
+
+    return c;
 }
--- src/chunk.h
+++ src/chunk.h
@@ -7,13 +7,24 @@
 
 typedef struct chunk
 {
-    int inode;
-    int ind;
-    char data[DATA_SIZE];
+    int _NOT_USED;
+
     int next_chunk_inode;
+    char data[DATA_SIZE];
+    int ind;
+    int inode;
 } chunk;
 
+/* Creates empty chunk */
 int chunk_create(memcached *);
+
+/* Initializes chunk */
 void chunk_init(chunk *, memcached *);
+
+/* Writes data into chunk */
+int chunk_write(chunk *, void *, int, memcached *);
+
+/* Return chunk with given inode */
+chunk *chunk_mmch_getchunk(int inode, memcached *m);
 
 #endif // !DATA_H
