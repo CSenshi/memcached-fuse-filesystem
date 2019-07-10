7a36b3b93b2520813cb87085f3987ff5d8a0d22d
CSenshi <saba.pochkhua@gmail.com>
2019-07-10 06:49:51 +0400

    divide printing for FS and memcaced

--- src/debug.h
+++ src/debug.h
@@ -4,11 +4,22 @@
 #include <stdio.h>
 
 #define DEBUG 1
+#define DEBUG_FS 1
 
 __fortify_function int
 _debug_print(const char *__restrict __fmt, ...)
 {
 #if (DEBUG)
+    return __printf_chk(__USE_FORTIFY_LEVEL - 1, __fmt, __va_arg_pack());
+#else
+    return 0;
+#endif
+}
+
+__fortify_function int
+_debug_print_FS(const char *__restrict __fmt, ...)
+{
+#if (DEBUG_FS)
     return __printf_chk(__USE_FORTIFY_LEVEL - 1, __fmt, __va_arg_pack());
 #else
     return 0;
