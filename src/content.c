6171c4ef4d858a5bab0b011ada0fd33e1ee7d2ca
CSenshi <saba.pochkhua@gmail.com>
2019-07-11 07:24:53 +0400

    implement content write methods

--- src/content.c
+++ src/content.c
@@ -60,18 +60,30 @@ int content_read(content *cn, int off_t, int size, char *buf, memcached *m)
 
 int content_write(content *cn, int off_t, int size, char *buf, memcached *m)
 {
-    int dirr_ind = off_t / C_DIRR, ind = off_t % C_DIRR;
-    chunk *c = _content_get_chunk(dirr_ind, cn, m);
-    return chunk_write(c, buf, size, m);
+    if (size == 0)
+        return 0;
+
+    chunk *c = _content_get_chunk(off_t, cn, m);
+    int written_bytes = chunk_write(c, buf, size, m);
+    cn->size += written_bytes;
+    memcached_replace_struct(m, int_to_str(cn->inode), cn, sizeof(struct content), 0, MM_CON);
+    return written_bytes;
 }
+
 int content_append(content *cn, int size, char *buf, memcached *m)
 {
-    return content_write(cn, cn->size, size, buf, m);
+    int total = 0, written_bytes = 0;
+    while (written_bytes = content_write(cn, cn->size, size - total, buf + total, m),
+           written_bytes > 0)
+    {
+        total += written_bytes;
+    }
+    return total;
 }
 
 int content_read_full_chunk(content *cn, int ch_num, char *buf, memcached *m)
 {
-    chunk *ch = _content_get_chunk(ch_num, cn, m);
+    chunk *ch = _content_get_chunk(DATA_SIZE * ch_num, cn, m);
     memcpy(buf, ch->data, ch->ind);
     return ch->ind;
 }
@@ -90,11 +102,16 @@ content *content_mmch_getcontent(int inode, memcached *m)
 
 chunk *_content_get_chunk(int n, content *cn, memcached *m)
 {
-    if (n < C_DIRR)
+    if (n < DATA_SIZE * C_DIRR)
     {
+        n = n / DATA_SIZE;
         if (!cn->DIRR_inode[n])
             cn->DIRR_inode[n] = chunk_create(m);
         return chunk_mmch_getchunk(cn->DIRR_inode[n], m);
+    }
+    else
+    {
+        // ToDo Indirects
     }
 
     return NULL;
