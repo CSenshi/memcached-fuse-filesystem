d01be25de4505abf5a86e11c5d2737fb703bfc41
CSenshi <saba.pochkhua@gmail.com>
2019-07-13 23:05:08 +0400

    add files tests

--- src/checkers/check_file.sh
+++ src/checkers/check_file.sh
@@ -30,23 +30,97 @@ test1(){
 }
 
 test2(){
-    echo "Test 2 : Read/Write..."
+    echo "Test 2 : Read/Write from subdirectories..."
     
-    TEST_FILE="TEST2_FILE.txt"
-    TEST_CONTENT="1234567890-=qwertyuiop[]asdfghjklzxcvbnm,./"
+    TEST_FILE="TEST_FILE.txt"
+    TEST_CONTENT="PLEASE END MY SUFFERING!!!"
     
     echo "$CMD echo $TEST_CONTENT > $TEST_FILE"
     echo $TEST_CONTENT > $TEST_FILE
     
+    echo "$CMD cat $TEST_FILE"
+    cat $TEST_FILE
+    
+    echo "Done!"
+    echo
+}
+
+test3(){
+    echo "Test 3 : Create files inside directories"
+    
+    dir1="dir1"
+    dir2="$dir1/dir2"
+    dir3="$dir2/dir3"
+    
+    echo "$CMD mkdir $dir1"
+    mkdir $dir1
+    echo "$CMD mkdir $dir2"
+    mkdir $dir2
+    echo "$CMD mkdir $dir3"
+    mkdir $dir3
+    
+    TEST_FILE="$dir3/DONT_OPEN.txt"
+    TEST_CONTENT="You Rebel!!!"
+    
+    echo "$CMD echo TEST_CONTENT > $TEST_FILE"
+    echo $TEST_CONTENT > $TEST_FILE
     
     echo "$CMD cat $TEST_FILE"
     cat $TEST_FILE
     echo
+    
+    echo "Done!"
+    echo
 }
 
 
-# Base Test Make 10 files
-test1 10
+test4(){
+    echo "Test 4 : Write/Append Data"
+    
+    TEST_FILE="test_append.txt"
+    
+    TEST_CONTENT="KILL!!!"
+    echo "$CMD echo TEST_CONTENT >> $TEST_FILE"
+    echo $TEST_CONTENT >> $TEST_FILE
+    
+    TEST_CONTENT="ME!!!"
+    echo "$CMD echo TEST_CONTENT >> $TEST_FILE"
+    echo $TEST_CONTENT >> $TEST_FILE
+    
+    TEST_CONTENT="NOW!!!"
+    echo "$CMD echo TEST_CONTENT >> $TEST_FILE"
+    echo $TEST_CONTENT >> $TEST_FILE
+    
+    TEST_CONTENT="FAST!!!"
+    echo "$CMD echo TEST_CONTENT >> $TEST_FILE"
+    echo $TEST_CONTENT >> $TEST_FILE
+    
+    echo "$CMD cat $TEST_FILE"
+    cat $TEST_FILE
+    echo
+    
+    echo "Done!"
+    echo
+}
 
-# Read and Write from file
-test2
+run_test(){
+    # Base Test Make 10 files
+    test1 10
+    
+    # Read and Write from file
+    test2
+    
+    # Write data inside directories
+    test3
+    
+    # Write and append data
+    test4
+}
+
+
+START=$(date +%s.%N)
+run_test
+END=$(date +%s.%N)
+DIFF=$(echo "$END - $START" | bc)
+
+echo "Elapsed time : $DIFF seconds"
